9cf7a17e0741e3d3cd2f7bf2fac00bbcbfda54d7
unknown <raissa.bez08@outlook.com>
2025-05-12 14:21:03 -0300

    Atividade dois

--- while.js
+++ while.js
@@ -1,7 +1,15 @@
-let num=0; // Criação da variável "num" e atribuição de valor a 0, para que quando o usuário digite um número, ele seja validado de maneira correta
+let nota = 0; // Criação da variável "nota" e atribuição de valor a 0, para que quando o usuário digite um número, ele seja validado de maneira correta
+let soma = 0;
+let i = 0;
 do {
-    num = parseInt(prompt("Por favor, digite um número")); // A variável "num" recebe um novo valor que seja inteiro, de acordo com aquilo que o usuário digitar
-    if (isNaN(num) || num <= 0) {
-        alert("Este número não é válido, digite novamente"); // Validação do número caso ele seja negativo ou não seja um número, a caixa de texto pede ao usuário para digitar novamente
+    nota = parseFloat(prompt("Por favor, digite suas notas")); // A variável "nota" recebe um novo valor que seja real, de acordo com aquilo que o usuário digitar
+
+    if (isNaN(nota) || nota <= 0 || nota > 10) {
+        alert("Este número não é válido para notas, digite novamente"); // Validação do número caso ele seja menor que 0 e maior que 10 ou não seja um número, a caixa de texto pede ao usuário para digitar novamente
+    } else {
+        soma = soma + nota;
+        i++;
     }
-} while ( num <0 || isNaN(num) ); // Condição da ação analisa se ele é negativo ou não é um número
+} while (nota > 0 || isNaN(nota)); // Condição da ação analisa se ele é menor que 0 ou não é um número
+let media = (soma / i);
+alert(`A média das notas é de: ${media}`);
